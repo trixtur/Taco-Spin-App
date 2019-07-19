814bb2189221ea3a779bd5e4d79ed737aa2b722d
Rodney Manning <rodneymanning@rainpos.com>
2019-07-19 15:55:19 -0600

    updated landing page

---
+++ spinning.php
@@ -0,0 +1,10 @@
+<div id="taco">
+	<img src="https://siterepository.s3.amazonaws.com/5581/taco.jpg" alt="Taco Spin" />
+</div>
+
+<div id="spins"></div>
+
+<div id="output">
+	<label for="totalSpins">Spins: </label> <span rel="totalSpinCount">?</span><br />
+	<label for="intervalSpeed">Interval: </label> <span rel="intervalSpeed">?</span>
+</div>
